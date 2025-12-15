949ce6e5d63137d63a144c7fa0a91000e4a34ae7
yanhui <lowyanhui04@gmail.com>
2025-12-15 15:06:23 +0800

    Updated code

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import render_template
 import config
 
@@ -6,13 +5,4 @@ app = config.connex_app
 app.add_api(config.basedir / "swagger.yml")
 
 if __name__ == "__main__":
-=======
-from flask import render_template
-import config
-
-app = config.connex_app
-app.add_api(config.basedir / "swagger.yml")
-
-if __name__ == "__main__":
->>>>>>> e2889fc1841252d7c44d92ac0068773bc7bfc1eb
     app.run(host="0.0.0.0", port=8000, debug=True)
--- config.py
+++ config.py
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import pathlib
-import connexion
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-
-basedir = pathlib.Path(__file__).parent.resolve()
-connex_app = connexion.App(__name__, specification_dir=basedir)
-
-app = connex_app.app
-
-# UPDATED FOR YOUR SPECIFIC DATABASE
-app.config["SQLALCHEMY_DATABASE_URI"] = (
-    "mssql+pyodbc:///?odbc_connect="
-    "DRIVER={ODBC Driver 18 for SQL Server};"
-    "SERVER=localhost;"
-    "DATABASE=MAL2018_Information_Management_Retrieval;"  # <-- The name from your screenshot
-    "UID=SA;"
-    "PWD=C0mp2001!;"               # Assuming you used the standard password
-    "TrustServerCertificate=yes;"
-    "Encrypt=yes;"
-)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SECRET_KEY"] = "super_secret_key_for_assignment_cw2"
-
-db = SQLAlchemy(app)
-=======
 import pathlib
 import connexion
 from flask_sqlalchemy import SQLAlchemy
@@ -49,5 +22,4 @@ app.config["SQLALCHEMY_DATABASE_URI"] = (
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
->>>>>>> e2889fc1841252d7c44d92ac0068773bc7bfc1eb
 ma = Marshmallow(app)
